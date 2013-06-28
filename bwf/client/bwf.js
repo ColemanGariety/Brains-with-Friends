05603967e35106b9321b7962bf86c24b123337c7
Jackson Gariety <personal@jacksongariety.com>
2013-06-27 22:15:55 -0700

    Adds all the basic events to input.js and requires them: mousedown, drag, mosemove, contextmenu, selectstart.

--- bwf/client/bwf.js
+++ bwf/client/bwf.js
@@ -4,11 +4,13 @@ goog.require('Dll');
 goog.require('lime.Director');
 goog.require('lime.Scene');
 goog.require('lime.Layer');
+goog.require('bwf.Input');
 
 // Is called from index.html after Google dependencies load
 bwf.start = function() {
   bwf.director = new lime.Director(document.body, window.innerWidth, window.innerHeight);
   bwf.scene = new lime.Scene();
+  bwf.director.replaceScene(bwf.scene);
 };
 
 goog.exportSymbol("bwf.start", bwf.start);
--- bwf/client/view/input.js
+++ bwf/client/view/input.js
@@ -1,1 +1,35 @@
 goog.provide('bwf.Input');
+
+// Mousedown
+goog.events.listen(document, goog.events.EventType.MOUSEDOWN, function(event) {
+  switch (event.event_.which) {
+    case 1: // Left-click
+      alert('Left-click');
+      break;
+    case 3: // Right-click
+      alert('Right-click');
+      break;
+  }
+});
+
+// Drag
+goog.events.listen(document, goog.events.EventType.DRAG, function(event) {
+  // Will be needed for inventory item re-arranging
+});
+
+// Mouse-move
+goog.events.listen(document, goog.events.EventType.MOUSEMOVE, function(event) {
+  // Update the game's knowledge of the mouse cursor
+  bwf.mouseX = event.clientX
+  bwf.mouseY = event.clientY
+});
+
+// Right-click
+goog.events.listen(document, goog.events.EventType.CONTEXTMENU, function(event) {
+  event.preventDefault(); // Kill the context menu
+});
+
+// Text selection
+goog.events.listen(document, goog.events.EventType.SELECTSTART, function(event) {
+  event.preventDefault(); // Kill the i-beam cursor
+});
