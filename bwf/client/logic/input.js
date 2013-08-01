bd6e6df184bdf584c68f57bea9ebfaf6fdcc582c
Kielan <ki.lemons@gmail.com>
2013-07-31 20:57:45 -0700

    pushing as I switch to virtual machine

--- bwf/client/logic/input.js
+++ bwf/client/logic/input.js
@@ -16,6 +16,7 @@ goog.events.listen(document, goog.events.EventType.MOUSEDOWN, Input.mousedown);
 
 //there are 12 frames in the walking animation, which cannot be divided evenly
 //by 1 second, there's some testing that needs to be done as far as the anim loop
+for (i = 0; i < 2;) {
 switch (event_.which) {
       var anim = new lime.animation.KeyframeAnimation().setDelay(.1)
       .addFrame(legAni1.png).addFrame(legAni2.png).addFrame(legAni3.png)
@@ -23,26 +24,58 @@ switch (event_.which) {
       .addFrame(legAni7.png).addFrame(legAni8.png).addFrame(legAni9.png)
       .addFrame(legAni10.png).addFrame(legAni11.png).addFrame(legAni12.png);
 
-  case 87: function moveUp(){
-    lime.animation.MoveBy(0,ya+10)
+  case 87: function moveUp() {
+    lime.animation.MoveBy(0,ya+10);
     sprite.runAction(anim);
+    console.log(agruments[0])
+    console.log(arguments[1])
   }
-    function moveUp()
+    function moveUp();
     break;
-  case 65: function moveLeft(){
-    lime.animation.MoveBy(xa-10,0)
-    sprite.runAction(anim).rotateBy(270);
+  case 65: function moveLeft() {
+    lime.animation.MoveBy(xa-10,0);
+    sprite.runAction(anim).RotateTo(270);
   }
     break;
-  case 83: function moveDown(){
+  case 83: function moveDown() {
     lime.animation.MoveBy(0,ya-10)
-    sprite.runAction(anim).rotateBy(180);
+    sprite.runAction(anim).RotateTo(180);
   }
     break;
-  case 68: function MoveRight(){
+  case 68: function moveRight() {
     lime.animation.MoveBy(xa+10,0)
-    sprite.runAction(anim).rotateBy(90);
+    sprite.runAction(anim).RotateTo(90);
+  }
     break;
+  case 87 + 65: function wa() {
+    lime.animation.MoveBy(-5, 5)
+    sprite.runAction(anim).RotateTo(135)
+  }
+    break;
+  }
+  case 65 + 83: function as() {
+    lime.animation.MoveBy(-5, -5)
+    sprite.runAction(anim).RotateTo(225)
+    break;
+  }
+  case 83 + 68: function sd() {
+    lime.animation.MoveBy(5, -5)
+    sprite.runAction(anim).RotateTo(315)
+    break;
+  }
+  case 65 + 87: function dw() {
+    lime.animation.MoveBy(5, 5)
+    sprite.runAction(anim).RotateTo(45)
+    break;
+  }
+  }
+  else {
+    function negotiate() {
+      arguments[0] + arguments[0]
+      arguments[1] + arguments[1]
+    }
+
+  }
 }
 //should there be a return statement for every case or is the line below defunct?
 return goog.events.KeyCodes.isCharacterKey(87);
@@ -79,4 +112,3 @@ Input.selectstart = function(event) {
   return false;
 }
 goog.events.listen(document, goog.events.EventType.SELECTSTART, Input.selectstart);
-
--- bwf/shared/logic/data/actor.js
+++ bwf/shared/logic/data/actor.js
@@ -1,13 +1,19 @@
 goog.provide('Actor');
+goog.require('Input');
 
 var Actor = new Class({
   constructor: function() {
+var xa = 100;
+var ya = 100;
+var target = function() {
+	//passing the variables from line 63 of input.js into these params is legal?
+	Math.atan2( Clienty, Clientx );
+	console.log(Math.atan2);
 //legs
 lime.Sprite().setSize(40,36).setFill('/legs/legAni2.png').setPosition(xa, ya); 
 //torso
-lime.Sprite().setSize(40,36).setFill().setPosition(xa, ya);
-var xa = 100;
-var ya = 100;
+lime.Sprite().setSize(40,36).setFill('/Torso/Torso.png').setPosition(xa, ya).RotateTo(target);
   }
+}
 });
 
