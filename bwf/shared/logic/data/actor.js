d0f26bb0d73f3e4ee94df6d5df4aa52e3b079a67
Jackson Gariety <personal@jacksongariety.com>
2013-08-20 18:30:18 -0700

    Adds some legs.

--- bwf/shared/logic/data/actor.js
+++ bwf/shared/logic/data/actor.js
@@ -3,20 +3,14 @@ goog.require('Input');
 
 var Actor = new Class({
   constructor: function() {
-var xa = 100;
-var ya = 100;
-var target = function() {
-	//passing the variables from line 63 of input.js into these params is legal?
-//	Math.atan2( Clienty, Clientx );
-//	console.log(Math.atan2);
-//legs
-var legs = new lime.Sprite().setSize(40,36).setFill('/legs/legAni2.png').setPosition(xa, ya); 
-layer.appendChild(legs);
-//torso
-/*
-var legs = new lime.Sprite().setSize(40,36).setFill('/Torso/Torso.png').setPosition(xa, ya).RotateTo(target);
-layer.appendChild(legs);
+    var xa = 100
+      , ya = 100
+      , layer = new lime.Layer()
+      , legs = new lime.Sprite().setSize(40,36).setFill('/assets/display/actors/legs/foo.png').setPosition(xa, ya)
+    
+    // Add legs to screen
+    client.scene.appendChild(layer);
+    layer.appendChild(legs);
+    
   }
-}
 });
-*/
