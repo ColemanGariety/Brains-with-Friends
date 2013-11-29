302f49bc7bd707a5a9889d7f9d87777943a22716
Jackson Gariety <personal@jacksongariety.com>
2013-11-28 16:09:06 -0800

    whoops

---
+++ bwf/native/Brains with Friends/Brains with Friends/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Brains with Friends
+//
+//  Created by Jackson Gariety on 10/5/13.
+//  Copyright (c) 2013 bwf. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
